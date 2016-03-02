6643658b2c3c2bc090c661427daf6eea06170198
yangfeng <519364805@qq.com>
2016-03-02 18:23:45 +0800

    修改redisTemplate类

--- dubbo-dal/src/main/java/com/jaking/dubbo/dal/cache/RedisTemplateExtension.java
+++ dubbo-dal/src/main/java/com/jaking/dubbo/dal/cache/RedisTemplateExtension.java
@@ -1,6 +1,25 @@
 package com.jaking.dubbo.dal.cache;
 
-public class RedisTemplateExtension {
-	public RedisTemplateExtension() {
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisTemplateExtension<K, V> {
+	private RedisTemplate<K, V> redisTemplate;
+
+	public RedisTemplateExtension(RedisTemplate<K, V> redisTemplate) {
+		this.redisTemplate = redisTemplate;
 	}
+
+	private RedisTemplate<K, V> getTemplate() {
+		return this.redisTemplate;
+	}
+	
+	private ListOperations<K, V> opList(){
+		return this.redisTemplate.opsForList();
+	}
+	
+	private Long size(K key){
+		return opList().size(key);
+	}
+	
 }
